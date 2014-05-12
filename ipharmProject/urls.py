2ea592537926d85a9cbd751d27a58b48bc0a6e42
Alfred <alfredayibonte>
2014-05-12 23:21:35 +0000

    added a change password form

--- ipharmProject/urls.py
+++ ipharmProject/urls.py
@@ -12,6 +12,17 @@ urlpatterns = patterns(
     (r'^facebook/', include('django_facebook.urls'),),
     (r'^accounts/', include('django_facebook.auth_urls', namespace='accounts')),
     (r'^user/', include('users.urls', namespace='users')),
-    (r'^pharmacy/', include('pharmacies.urls', namespace='pharmacies'))
+    (r'^pharmacy/', include('pharmacies.urls', namespace='pharmacies')),
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    # Support old style base36 password reset links; remove in Django 1.7
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm_uidb36'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
 
 )
