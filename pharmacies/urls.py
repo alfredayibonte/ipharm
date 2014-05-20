e8a63ca2421bace0dd723a11d1aa480680710fe7
Alfred <alfredayibonte>
2014-05-20 09:41:20 +0000

    email integration with mandrill

--- pharmacies/urls.py
+++ pharmacies/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from pharmacies import views as pharmacy
+import views as pharmacy
 
 urlpatterns = patterns(
     '',
-    url(r'^main/$', pharmacy.Main.as_view(), name='main')
+    url(r'^main/$', pharmacy.Main.as_view(), name='main'),
+    url(r'^email/$', pharmacy.Email.as_view(), name='email'),
 )
